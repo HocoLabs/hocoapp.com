4a100d49cf75082ed374c81a75b7583bd96a2d8c
Josh <jcjohnson77@gmail.com>
2022-09-12 20:45:44 -0400

    hoco-2 Add email placeholder

--- src/components/Waitlist/Waitlist.tsx
+++ src/components/Waitlist/Waitlist.tsx
@@ -38,7 +38,7 @@ export const Waitlist = () => {
       <CardContent>
         <Typography>Join the waitlist for early access to Hoco!</Typography>
         <div className={styles.waitlistInput}>
-          <Input onChange={(e) => setEmail(e.target.value)} value={email} disabled={loading} />
+          <Input onChange={(e) => setEmail(e.target.value)} value={email} disabled={loading} placeholder="Email" />
           <Button color="primary" variant="contained" onClick={onSubmit} disabled={email.trim().length === 0 || loading}>Join</Button>
         </div>
         {responseDisplay}
